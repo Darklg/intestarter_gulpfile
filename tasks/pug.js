8f4d1ceafb5205f624a3e883f5bd37bfee4f10b3
Kevin <kevin@rocher-dev.com>
2024-06-03 15:27:06 +0200

    v 0.3.1
    Pug : handle extra vars.

--- tasks/pug.js
+++ tasks/pug.js
@@ -7,9 +7,22 @@ const replace = require('gulp-replace');
 const pug = require('gulp-pug');
 const glob = require('glob');
 
-module.exports = function(p, pug_views, pug_files, svg_files, js_folder, css_folder) {
+module.exports = function(p, pug_views, pug_files, svg_files, js_folder, css_folder, extras) {
     return function intestarter_gulp_pug() {
-    'use strict';
+        'use strict';
+
+        var _locals = {
+            icons: _icons,
+            jsFiles: glob.sync(js_folder + '/*.js'),
+            cssFiles: glob.sync(css_folder + '/*.css'),
+            package: p
+        };
+
+        if (extras) {
+            for(var _item in extras) {
+                _locals[_item] = extras[_item];
+            }
+        }
 
         /* List icons */
         var _icons = glob.sync(svg_files).map(function(item) {
@@ -19,12 +32,7 @@ module.exports = function(p, pug_views, pug_files, svg_files, js_folder, css_fol
         return gulp
             .src([pug_views + '*.pug'])
             .pipe(pug({
-                locals: {
-                    icons: _icons,
-                    jsFiles: glob.sync(js_folder + '/*.js'),
-                    cssFiles: glob.sync(css_folder + '/*.css'),
-                    package: p
-                },
+                locals: _locals,
                 doctype: 'html',
                 pretty: false
             }))
