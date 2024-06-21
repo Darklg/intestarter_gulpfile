b280994f197a1c8f3722a0db734ad509a73daab7
Kevin <kevin@rocher-dev.com>
2024-06-21 17:40:28 +0200

    v 0.3.2
    Pug : fix invalid icon var

--- tasks/pug.js
+++ tasks/pug.js
@@ -11,6 +11,11 @@ module.exports = function(p, pug_views, pug_files, svg_files, js_folder, css_fol
     return function intestarter_gulp_pug() {
         'use strict';
 
+        /* List icons */
+        var _icons = glob.sync(svg_files).map(function(item) {
+            return item.split('/').reverse()[0].replace('.svg', '');
+        });
+
         var _locals = {
             icons: _icons,
             jsFiles: glob.sync(js_folder + '/*.js'),
@@ -23,11 +28,6 @@ module.exports = function(p, pug_views, pug_files, svg_files, js_folder, css_fol
                 _locals[_item] = extras[_item];
             }
         }
-
-        /* List icons */
-        var _icons = glob.sync(svg_files).map(function(item) {
-            return item.split('/').reverse()[0].replace('.svg', '');
-        });
 
         return gulp
             .src([pug_views + '*.pug'])
